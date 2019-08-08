c9cf28f7618690e904e4fa3398f7390f9b66eaeb
Fábio Lira <fabiolira1910@hotmail.com>
2019-08-08 20:53:01 -0300

    countNumbers
    entire project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace countNumbers{
+
+	class CountNumbers
+	{
+		public int countNumbers(int []arr, int lessThan){
+			int lessThanCount = 0;
+			List<int> auxList = new List<int>();
+			
+			for(int i = 0; i < arr.Length; i++){
+				auxList.Add(arr[i]);
+				if(arr[i] < lessThan){
+					lessThanCount++;
+				}
+			}
+			
+			auxList.Sort();
+			return lessThanCount;
+		}
+	}
+	
+	class Program{
+		public static void Main(){
+			CountNumbers cn = new CountNumbers();
+			Console.WriteLine("Amostra = [1, 2, 3, 4, 5, 6]. lessThan = 4. ");
+			Console.WriteLine("Execução retornou: {0}", cn.countNumbers(new int [] {1,2,3,4,5,6}, 4));
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ countNumbersTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace countNumbers
+{
+	[TestFixture]
+	public class CountNumbersTest
+	{
+		[Test]
+		public void countNumbers()
+		{
+			CountNumbers cn = new CountNumbers();
+			Assert.AreEqual(3, cn.countNumbers(new int [] {1,2,3,4,5,6}, 4));
+			
+		}
+	}
+}
